ba456571563aa4adf0ca69cd3973a8d18ebba646
DaryaYa <lyalyaka@mail.ru>
2020-03-11 16:19:45 +0300

    feat: class .active & modal

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('jhdfjhghsdjk')
+'use strict';
+
+const TOP_MENU = document.getElementById('top-menu');
+const PLATES = document.getElementById('plates');
+const SUBMIT = document.querySelector('.submit-button');
+const MESSAGE = document.getElementById('message');
+const closeMessage = document.getElementById('close-message');
+
+TOP_MENU.addEventListener('click', (event) => {
+    const target = event.target;
+    TOP_MENU.querySelectorAll('li').forEach(element => element.classList.remove('active'));   
+    target.closest('li').classList.add('active');   
+});
+
+PLATES.addEventListener('click', (ev) => {
+    PLATES.querySelectorAll('div').forEach(el => el.querySelector('img').classList.remove('active-border'));
+    ev.target.classList.add('active-border');
+});
+
+SUBMIT.addEventListener('click', (eve) => {
+    eve.preventDefault();
+    MESSAGE.style.display = 'block';
+});
+
+closeMessage.addEventListener('click', (e) => {
+    e.target.closest('div').style.display = 'none';
+})
